a57ec353ec4e12c3f4e20f6ff7ee0183194776e1
zxwing99 <zacharysnorton@gmail.com>
2020-12-26 18:16:16 -0800

    testing
    testing this

--- Main.java
+++ Main.java
@@ -1,2 +1,23 @@
-class 
-public static void main(String[] args) {
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Main extends JFrame{
+	static Container c;
+
+	public static void main(String[] args) {
+		Main panel = new Main();
+		panel.setBounds(100, 100, 800, 600); 
+		panel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel.setResizable(false);
+		c = panel.getContentPane(); // *****
+		c.setBackground(Color.BLACK);
+		panel.setBackground(Color.BLACK);
+		panel.setVisible(true); // ***
+		c.setVisible(true);
+	}
+}
