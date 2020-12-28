873b37ee3abf4befd6dde964e34619b5d4a51693
zxwing99 <zacharysnorton@gmail.com>
2020-12-27 22:58:49 -0800

    Update Map

--- Assets/Map.java
+++ Assets/Map.java
@@ -1,23 +1,62 @@
 package Assets;
 
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.LinkedList;
 
 import pieces.*;
 
 public class Map implements ActionListener{
 	
-	private walls[] wals;
-	private enimies[] enimies;
-	private Player player;
+	private LinkedList<walls[]> walls;
+	private int wallCount, level, enimies;
+	private int wallSize;
 	
-	public Map() {
-		
+	public Map(int wallSize) {
+		this.wallSize = wallSize;
+		wallCount = 1;
+		level = 0;
+		enimies = 0;
+		for (int i = 0; i < 5; i++) {
+			createWall(20*i);
+		}
 	}
 	
+	public void createWall(int x) {
+		walls[] newWall = new walls[10];
+		if (level % 6 == 0 && wallCount <= 7) {
+			wallCount++;
+		}
+		if (level % 10 == 0 && enimies <= 3) {
+			enimies ++;
+		}
+		int wallc = 0;
+		int enimiec = 0;
+		while (wallc < wallCount) {
+			int i = (int) (Math.random()*10);
+			if (newWall[i] == null) {
+				newWall[i] = new pieces.walls(x, wallSize*i, 20, wallSize);
+				wallc++;
+			}
+		}
+		while (enimiec < enimies) {
+			int i = (int) (Math.random()*10);
+			if (newWall[i] == null) {
+				newWall[i] = new pieces.enimies(x, wallSize*i, 20, wallSize);
+				enimiec++;
+			}
+		}
+		this.walls.add(newWall);
+		level++;
+	}
 	
-	public void update() {
-		
+	public void draw(Graphics dp) {
+		for(walls[] bigWall : this.walls) {
+			for (walls wall : bigWall) {
+				wall.paint(dp);
+			}
+		}
 	}
 
 
