917128aee8e8d375cdfdec9e4705168ba0f8f0b5
zxwing99 <zacharysnorton@gmail.com>
2020-12-27 22:14:57 -0800

    map

--- Assets/Map.java
+++ Assets/Map.java
@@ -1,14 +1,29 @@
 package Assets;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import pieces.*;
 
-public class Map {
+public class Map implements ActionListener{
 	
 	private walls[] wals;
 	private enimies[] enimies;
 	private Player player;
 	
 	public Map() {
+		
+	}
+	
+	
+	public void update() {
+		
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 
