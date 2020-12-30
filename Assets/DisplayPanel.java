4a7a09bed1fc373290e51e2fe249597f72beb50a
Jesse <jlutannn@gmail.com>
2020-12-29 18:33:35 -0800

    Added score display, game over screen, and some crash fixes

--- Assets/DisplayPanel.java
+++ Assets/DisplayPanel.java
@@ -12,41 +12,59 @@ import pieces.*;
 public class DisplayPanel extends PApplet implements ActionListener {
 
 	private Map map;
+	private int score;
 	private Timer timer, playerTimer;
 	private Player player;
 	private int lazerY, lazerX;
 
 	public DisplayPanel() {
 		player = new pieces.Player(0, 550 / 2);
-		map = new Map(50, player);
-		// player = new Player(0, 550 / 2);
-		timer = new Timer(15, map);
-		playerTimer = new Timer(30, this);
-		timer.start();
-		playerTimer.start();
-		runSketch();
+		map = new Map(50);
+		// timer = new Timer(15, map);
+		// playerTimer = new Timer(30, this);
+		// timer.start();
+		// playerTimer.start();
+		
+		runSketch(); // instantiates drawing surface
 	}
 
 	public void setup() {
-		frameRate(30);
+		frameRate(60);
+		textAlign(PConstants.CENTER);
+		textSize(20);
 	}
 
 	// runs at every game tick
 	public void draw() {
 		super.background(255);
+		map.draw(this, player);
+		score = map.getLevel() - 4;
+
 		if (player.isDead()) {
 			player.setRect(0, 0, 0, 0);
 			stop();
-			timer.stop();
-			playerTimer.stop();
+			// timer.stop();
+			// playerTimer.stop();
+			
+			textSize(32);
+			
+			fill(0);
+			text("Game Over!\nYour Score: " + score, this.width / 2, this.height / 2 );
 		}
 
-		map.draw(this);
+		lazerY = player.shoot();
+		lazerX = map.getShot(lazerY);
+
 		player.paint(this);
 		player.drawHealth(this);
-
-		if(!player.isDead())
+		if (!player.isDead()) {
+			fill(0);
+			if (score < 0)
+				text("Score: " + 0, this.width / 2, 20);
+			else
+				text("Score: " + score, this.width / 2, 20);
 			line(30, lazerY, lazerX, lazerY);
+		}
 	}
 
 	public void keyPressed() {
@@ -65,7 +83,6 @@ public class DisplayPanel extends PApplet implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		lazerY = player.shoot();
-		lazerX = map.getShot(lazerY);
+
 	}
 }
